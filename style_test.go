faf1a8848b1b389ee84c5d9593e2cc157d19eb69
Kenneth Shaw <kenneth.shaw@knq.io>
2019-01-31 08:45:28 +0700

    Adding psql output to style test

--- style_test.go
+++ style_test.go
@@ -8,6 +8,7 @@ import (
 
 func TestFormats(t *testing.T) {
 	for _, n := range []string{
+		"psql",
 		"unaligned",
 		"aligned",
 		//"wrapped",
@@ -20,11 +21,23 @@ func TestFormats(t *testing.T) {
 		"csv",
 	} {
 		buf := new(bytes.Buffer)
-		if err := EncodeAll(buf, rs(), map[string]string{
-			"format": n,
-		}); err != nil {
-			t.Fatalf("expected no error when encoding format %q, got: %v", n, err)
+
+		if n == "psql" {
+			if err := psqlEncodeAll(buf, rs()); err != nil {
+				if err == errPsqlConnNotDefined {
+					t.Logf("PSQL_CONN not defined, skipping psql query")
+					continue
+				}
+				t.Fatalf("unable to run psql, got: %v", err)
+			}
+		} else {
+			if err := EncodeAll(buf, rs(), map[string]string{
+				"format": n,
+			}); err != nil {
+				t.Fatalf("expected no error when encoding format %q, got: %v", n, err)
+			}
 		}
+
 		log.Printf("format %q:\n%s", n, buf.String())
 	}
 }
--- util_test.go
+++ util_test.go
@@ -1,7 +1,16 @@
 package tblfmt
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 type rset struct {
@@ -70,7 +79,11 @@ func (r *rset) Next() bool {
 
 func (r *rset) Scan(vals ...interface{}) error {
 	for i := range vals {
-		vals[i] = &r.vals[r.rs][r.pos][i]
+		x, ok := vals[i].(*interface{})
+		if !ok {
+			return fmt.Errorf("scan for col %d expected *interface{}, got: %T", i, vals[i])
+		}
+		*x = r.vals[r.rs][r.pos][i]
 	}
 	r.pos++
 	return nil
@@ -81,18 +94,108 @@ func (r *rset) NextResultSet() bool {
 	return r.rs < len(r.vals)
 }
 
+var errPsqlConnNotDefined = errors.New("PSQL_CONN not defined")
+
+// psqlEncodeAll does a values query for each of the values in the result set,
+// writing captured output to the writer.
+func psqlEncodeAll(w io.Writer, resultSet ResultSet) error {
+	dsn := os.Getenv("PSQL_CONN")
+	if len(dsn) == 0 {
+		return errPsqlConnNotDefined
+	}
+
+	var err error
+
+	if err = psqlEncode(w, resultSet, dsn); err != nil {
+		return err
+	}
+
+	for resultSet.NextResultSet() {
+		if _, err = w.Write(newline); err != nil {
+			return err
+		}
+
+		if err = psqlEncode(w, resultSet, dsn); err != nil {
+			return err
+		}
+	}
+
+	if _, err = w.Write(newline); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 const (
-	asciiBorder1 = ` author_id | name                  | z 
------------+-----------------------+---
-        14 | a	b	c	d  | x 
-        15 | aoeu                 +|   
-           | test                 +|   
-           |                       |   
-        16 | foo\bbar              |   
-        17 | 袈	袈		袈 |   
-        18 | a	b	\r        +| a+
-           | 	a                  |   
-        19 | 袈	袈		袈+|   
-           |                       |   
-(6 rows)`
+	psqlValuesQuery = `SELECT * FROM (
+  VALUES%s
+) AS t (author_id, name, z);`
 )
+
+// psqlEncode does a single value query using psql, writing the catpured output
+// to the writer.
+func psqlEncode(w io.Writer, resultSet ResultSet, dsn string) error {
+	var err error
+
+	// read values
+	var vals string
+	var i int
+	for resultSet.Next() {
+		var id, name, z interface{}
+		if err = resultSet.Scan(&id, &name, &z); err != nil {
+			return err
+		}
+		var extra string
+		if i != 0 {
+			extra = ","
+		}
+
+		n := name.(string)
+		vals += fmt.Sprintf("%s\n    (%v,E'%s', %s)", extra, id, psqlEsc(n), psqlEnc(n, z))
+
+		i++
+	}
+	if err = resultSet.Err(); err != nil {
+		return err
+	}
+
+	// exec
+	stdout := new(bytes.Buffer)
+	q := fmt.Sprintf(psqlValuesQuery, vals)
+	cmd := exec.Command("psql", dsn, "-q")
+	cmd.Stdin, cmd.Stdout = bytes.NewReader([]byte(q)), stdout
+	if err = cmd.Run(); err != nil {
+		return err
+	}
+
+	_, err = w.Write(bytes.TrimRightFunc(stdout.Bytes(), unicode.IsSpace))
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(newline)
+	return err
+}
+
+// psqlEsc escapes a string as a psql string.
+func psqlEsc(s string) string {
+	s = strings.Replace(s, "\n", `\n`, -1)
+	s = strings.Replace(s, "\r", `\r`, -1)
+	s = strings.Replace(s, "\t", `\t`, -1)
+	s = strings.Replace(s, "\b", `\b`, -1)
+	s = strings.Replace(s, "袈", `\u8888`, -1)
+	return s
+}
+
+// psqlEnc encodes v based on n.
+func psqlEnc(n string, v interface{}) string {
+	if n != "javascript" && n != "slice" {
+		return "NULL"
+	}
+	buf, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	s := strconv.QuoteToASCII(string(buf))
+	return "E'" + s[1:] + s[:len(s)-1] + "'"
+}
